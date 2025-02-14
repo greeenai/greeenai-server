4551006dfda2eba72dd1bfe87464df3496977805
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2025-02-14 13:56:12 +0900

    feat: 멤버 도메인 구현 (#5)

---
+++ src/main/java/com/greeenai/greeenai/domain/member/domain/Member.java
@@ -0,0 +1,34 @@
+package com.greeenai.greeenai.domain.member.domain;
+
+import java.time.LocalDateTime;
+
+import com.greeenai.greeenai.domain.common.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+	@Id
+	private Long id;
+
+	private String name;
+
+	private String email;
+
+	private String authId;
+
+	@Enumerated(EnumType.STRING)
+	private OAuthProvider oAuthProvider;
+
+	private LocalDateTime lastLoginAt;
+
+}
