845d829799dc4e496e4fd379092beb057b0367f3
chosw1002@naver.com <chosw1002@naver.com>
2025-02-18 13:22:48 +0900

    feat: SecurityConfig에 jwtAuthenticationFilter 추가

--- src/main/java/com/greeenai/greeenai/global/config/SecurityConfig.java
+++ src/main/java/com/greeenai/greeenai/global/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.greeenai.greeenai.global.security.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +16,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 		http
 			.authorizeHttpRequests(authorizeRequests ->
 				authorizeRequests
