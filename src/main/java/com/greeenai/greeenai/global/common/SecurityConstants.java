8114adb6219f66544c09db843050f138105832ad
chosw1002@naver.com <chosw1002@naver.com>
2025-02-18 15:19:04 +0900

    feat: jwt 인증 및 재발급 로직 추가

---
+++ src/main/java/com/greeenai/greeenai/global/common/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.greeenai.greeenai.global.common;
+
+public class SecurityConstants {
+
+	public static final String ACCESS_TOKEN = "accessToken";
+	public static final String REFRESH_TOKEN = "refreshToken";
+
+	private SecurityConstants() {}
+}
---
+++ src/main/java/com/greeenai/greeenai/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.greeenai.greeenai.global.security;
+
+import static com.greeenai.greeenai.global.common.SecurityConstants.*;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.greeenai.greeenai.global.util.JwtUtil;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtil;
+	private final JwtService jwtService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String accessToken = jwtUtil.extractTokenFromCookie(request, ACCESS_TOKEN);
+		String refreshToken = jwtUtil.extractTokenFromCookie(request, REFRESH_TOKEN);
+
+		try {
+			Long memberId = jwtService.parseToken(ACCESS_TOKEN, accessToken);
+			setAuthenticationToContext(PrincipalDetails.from(memberId));
+		} catch (ExpiredJwtException e) {
+			Long memberId = jwtService.parseToken(REFRESH_TOKEN, refreshToken);
+			String newAccessToken = jwtService.generateToken(ACCESS_TOKEN, memberId);
+			jwtUtil.addTokenCookie(response, ACCESS_TOKEN, newAccessToken);
+			setAuthenticationToContext(PrincipalDetails.from(memberId));
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private void setAuthenticationToContext(UserDetails userDetails) {
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, null);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+}
