8bf02e7887590c1996112a83a47380c5b13514eb
ohhamma <judyham10634@naver.com>
2025-03-04 13:41:50 +0900

    feat: 인증 관련 ErrorCode 추가

--- src/main/java/com/greeenai/greeenai/global/error/exception/ErrorCode.java
+++ src/main/java/com/greeenai/greeenai/global/error/exception/ErrorCode.java
@@ -15,6 +15,15 @@ public enum ErrorCode {
 	QUERY_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "올바르지 않은 쿼리 타입 입니다."),
 	QUERY_PARAM_INVALID(HttpStatus.BAD_REQUEST, "올바르지 않은 쿼리 파라미터 값입니다."),
 	QUERY_PARAM_NOT_FOUND(HttpStatus.BAD_REQUEST, "쿼리 파라미터가 존재하지 않습니다."),
+
+	// Token
+	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+	TOKEN_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "토큰 생성 중 오류가 발생했습니다."),
+
+	// Authentication
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+	PERMISSION_DENIED(HttpStatus.FORBIDDEN, "권한이 거부되었습니다."),
 	;
 
 	private final HttpStatus httpStatus;
