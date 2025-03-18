8350ce7ebeb5f3c35e463b923e74814c47444daf
ohhamma <judyham10634@naver.com>
2025-03-18 11:28:38 +0900

    chore: spotless 적용

--- src/test/java/com/greeenai/greeenai/global/error/GlobalExceptionHandlerTest.java
+++ src/test/java/com/greeenai/greeenai/global/error/GlobalExceptionHandlerTest.java
@@ -1,7 +1,14 @@
 package com.greeenai.greeenai.global.error;
 
+import static com.greeenai.greeenai.global.error.exception.ErrorCode.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import com.greeenai.greeenai.global.error.exception.CustomException;
 import com.greeenai.greeenai.global.error.exception.ErrorCode;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -22,157 +29,150 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.greeenai.greeenai.global.error.exception.ErrorCode.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @ActiveProfiles({"test", "h2"})
 class GlobalExceptionHandlerTest {
 
-	@InjectMocks
-	private GlobalExceptionHandler globalExceptionHandler;
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
 
-	@Mock
-	private WebRequest webRequest;
+    @Mock
+    private WebRequest webRequest;
 
-	@BeforeEach
-	void setUp() {
-		MockitoAnnotations.openMocks(this);
-	}
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-	@Test
-	void handleMethodArgumentNotValid() {
-		MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
-		BindingResult bindingResult = mock(BindingResult.class);
-		List<FieldError> fieldErrors = new ArrayList<>();
-		fieldErrors.add(new FieldError("objectName", "field", "defaultMessage"));
-		when(ex.getBindingResult()).thenReturn(bindingResult);
-		when(bindingResult.getFieldErrors()).thenReturn(fieldErrors);
+    @Test
+    void handleMethodArgumentNotValid() {
+        MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+        List<FieldError> fieldErrors = new ArrayList<>();
+        fieldErrors.add(new FieldError("objectName", "field", "defaultMessage"));
+        when(ex.getBindingResult()).thenReturn(bindingResult);
+        when(bindingResult.getFieldErrors()).thenReturn(fieldErrors);
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentNotValid(
-				ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentNotValid(
+                ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(METHOD_ARGUMENT_INVALID.getMessage() + " {field=defaultMessage}", errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(METHOD_ARGUMENT_INVALID.getMessage() + " {field=defaultMessage}", errorResponse.message());
+    }
 
-	@Test
-	void handleHttpRequestMethodNotSupported() {
-		HttpRequestMethodNotSupportedException ex = new HttpRequestMethodNotSupportedException("POST");
+    @Test
+    void handleHttpRequestMethodNotSupported() {
+        HttpRequestMethodNotSupportedException ex = new HttpRequestMethodNotSupportedException("POST");
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHttpRequestMethodNotSupported(
-				ex, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHttpRequestMethodNotSupported(
+                ex, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED, webRequest);
 
-		assertEquals(HttpStatus.METHOD_NOT_ALLOWED, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(METHOD_NOT_SUPPORTED.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.METHOD_NOT_ALLOWED, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(METHOD_NOT_SUPPORTED.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleHandlerMethodValidationException() {
-		HandlerMethodValidationException ex = mock(HandlerMethodValidationException.class);
+    @Test
+    void handleHandlerMethodValidationException() {
+        HandlerMethodValidationException ex = mock(HandlerMethodValidationException.class);
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHandlerMethodValidationException(
-				ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHandlerMethodValidationException(
+                ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(QUERY_PARAM_INVALID.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(QUERY_PARAM_INVALID.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleMissingServletRequestParameter() {
-		MissingServletRequestParameterException ex = new MissingServletRequestParameterException("param", "String");
+    @Test
+    void handleMissingServletRequestParameter() {
+        MissingServletRequestParameterException ex = new MissingServletRequestParameterException("param", "String");
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMissingServletRequestParameter(
-				ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMissingServletRequestParameter(
+                ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(QUERY_PARAM_NOT_FOUND.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(QUERY_PARAM_NOT_FOUND.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleExceptionInternal() {
-		Exception ex = new Exception("Internal error");
+    @Test
+    void handleExceptionInternal() {
+        Exception ex = new Exception("Internal error");
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleExceptionInternal(
-				ex, null, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleExceptionInternal(
+                ex, null, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
 
-		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(INTERNAL_SERVER_ERROR.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(INTERNAL_SERVER_ERROR.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleHttpMessageNotReadable() {
-		HttpMessageNotReadableException ex = new HttpMessageNotReadableException("Message not readable");
+    @Test
+    void handleHttpMessageNotReadable() {
+        HttpMessageNotReadableException ex = new HttpMessageNotReadableException("Message not readable");
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHttpMessageNotReadable(
-				ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleHttpMessageNotReadable(
+                ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(HTTP_MESSAGE_NOT_READABLE.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(HTTP_MESSAGE_NOT_READABLE.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleCustomException() {
-		ErrorCode errorCode = INTERNAL_SERVER_ERROR;
-		CustomException ex = new CustomException(errorCode);
+    @Test
+    void handleCustomException() {
+        ErrorCode errorCode = INTERNAL_SERVER_ERROR;
+        CustomException ex = new CustomException(errorCode);
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleCustomException(ex);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleCustomException(ex);
 
-		assertEquals(errorCode.getHttpStatus(), responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(errorCode.getMessage(), errorResponse.message());
-	}
+        assertEquals(errorCode.getHttpStatus(), responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(errorCode.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleMethodArgumentTypeMismatchException() {
-		MethodArgumentTypeMismatchException ex = mock(MethodArgumentTypeMismatchException.class);
+    @Test
+    void handleMethodArgumentTypeMismatchException() {
+        MethodArgumentTypeMismatchException ex = mock(MethodArgumentTypeMismatchException.class);
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentTypeMismatchException(ex);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleMethodArgumentTypeMismatchException(ex);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(QUERY_TYPE_MISMATCH.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(QUERY_TYPE_MISMATCH.getMessage(), errorResponse.message());
+    }
 
-	@Test
-	void handleHttpClientErrorException() {
-		HttpClientErrorException ex = HttpClientErrorException.create(HttpStatus.BAD_REQUEST, "Bad Request", new HttpHeaders(), null, null);
+    @Test
+    void handleHttpClientErrorException() {
+        HttpClientErrorException ex =
+                HttpClientErrorException.create(HttpStatus.BAD_REQUEST, "Bad Request", new HttpHeaders(), null, null);
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleClientErrorException(ex);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleClientErrorException(ex);
 
-		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(ex.getResponseBodyAsString(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(ex.getResponseBodyAsString(), errorResponse.message());
+    }
 
-	@Test
-	void handleException() {
-		Exception ex = new RuntimeException("Unexpected error");
+    @Test
+    void handleException() {
+        Exception ex = new RuntimeException("Unexpected error");
 
-		ResponseEntity<Object> responseEntity = globalExceptionHandler.handleException(ex);
+        ResponseEntity<Object> responseEntity = globalExceptionHandler.handleException(ex);
 
-		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
-		ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
-		assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
-		assertEquals(INTERNAL_SERVER_ERROR.getMessage(), errorResponse.message());
-	}
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        ErrorResponse errorResponse = (ErrorResponse) responseEntity.getBody();
+        assertEquals(ex.getClass().getSimpleName(), errorResponse.className());
+        assertEquals(INTERNAL_SERVER_ERROR.getMessage(), errorResponse.message());
+    }
 }
