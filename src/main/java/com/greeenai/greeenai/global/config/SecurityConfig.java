deddcfbd0e4715644b2e8b7ef944364dc24f855a
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2025-03-24 18:21:56 +0900

    chore: 배포 자동화 (#30)
    * chore: 워크플로우 추가
    
    * chore: 도커 컴포즈 추가
    
    * chore: 도커 파일 추가
    
    * chore: 풀리퀘스트 생성 시 워크플로우 실행
    
    * remove: aws credentials 세팅 로직 제거
    
    * chore: spotless 설정 변경
    
    * test: 테스트 실행 시 test profile 활성화
    
    * fix: Base64 디코딩 추가
    
    * fix: Base64 디코딩 제거
    
    * fix: PostConstruct 제거
    
    * fix: 경로를 문자열로 수정
    
    * fix: scp 덮어쓰기 옵션 추가
    
    * fix: 타켓 수정
    
    * fix: 타켓 수정
    
    * fix: 타켓 수정
    
    * fix: 시큐리티 경로 수정
    
    * fix: 필터 로직 수정
    
    * fix: 기본 필터 설정
    
    * fix: http basic 설정
    
    * chore: pr 생성시 워크플로우 동작 제거

--- src/main/java/com/greeenai/greeenai/global/config/SecurityConfig.java
+++ src/main/java/com/greeenai/greeenai/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,13 +19,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers("/login")
+                .requestMatchers("/auth/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated());
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
--- src/main/java/com/greeenai/greeenai/global/error/exception/ErrorCode.java
+++ src/main/java/com/greeenai/greeenai/global/error/exception/ErrorCode.java
@@ -20,6 +20,7 @@ public enum ErrorCode {
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     TOKEN_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "토큰 생성 중 오류가 발생했습니다."),
+    TOKEN_TYPE_INVALID(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 토큰 타입입니다."),
 
     // Authentication
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
