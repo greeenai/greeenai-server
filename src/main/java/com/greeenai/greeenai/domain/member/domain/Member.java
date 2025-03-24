2a34470a2281af308befe6493f5aeeced24b5dfb
chosw1002@naver.com <chosw1002@naver.com>
2025-03-24 21:35:48 +0900

    fix: member의 pk에 auto increment 적용

--- src/main/java/com/greeenai/greeenai/domain/member/domain/Member.java
+++ src/main/java/com/greeenai/greeenai/domain/member/domain/Member.java
@@ -4,6 +4,8 @@ import com.greeenai.greeenai.domain.common.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
 public class Member extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
