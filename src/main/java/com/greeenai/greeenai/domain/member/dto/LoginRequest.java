ad6a46bacf9b1632913cae91af3252e8b58f725d
ohhamma <judyham10634@naver.com>
2025-03-25 15:54:44 +0900

    fix: enum validation을 NotNull로 변경

--- src/main/java/com/greeenai/greeenai/domain/member/dto/LoginRequest.java
+++ src/main/java/com/greeenai/greeenai/domain/member/dto/LoginRequest.java
@@ -2,9 +2,10 @@ package com.greeenai.greeenai.domain.member.dto;
 
 import com.greeenai.greeenai.domain.member.domain.OauthProvider;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @NotBlank String name,
         @NotBlank String email,
         @NotBlank String oauthId,
-        @NotBlank OauthProvider oAuthProvider) {}
+        @NotNull OauthProvider oAuthProvider) {}
