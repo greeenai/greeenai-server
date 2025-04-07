d46a3ac37689c5f2fc44cc274df85691b2013b98
ohhamma <judyham10634@naver.com>
2025-04-07 23:16:24 +0900

    refactor: 회원 탈퇴 테스트 제거

--- src/test/java/com/greeenai/greeenai/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/greeenai/greeenai/domain/member/service/MemberServiceTest.java
@@ -4,13 +4,10 @@ import static com.greeenai.greeenai.global.common.constant.TestConstants.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import com.greeenai.greeenai.domain.member.domain.Member;
-import com.greeenai.greeenai.domain.member.domain.OauthProvider;
 import com.greeenai.greeenai.domain.member.repository.MemberRepository;
 import com.greeenai.greeenai.global.util.MemberUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -32,21 +29,5 @@ class MemberServiceTest {
 
     @Nested
     @DisplayName("회원 탈퇴 테스트")
-    class WithdrawMemberTests {
-
-        @Test
-        @DisplayName("회원 탈퇴 요청을 하면 회원을 삭제한다")
-        void withdrawMember_shouldDeleteCurrentMember() {
-            // Given
-            Member currentMember = Member.create(TEST_NAME, TEST_EMAIL, TEST_OAUTH_ID, OauthProvider.APPLE);
-            when(memberUtil.getCurrentMember()).thenReturn(currentMember);
-
-            // When
-            memberService.withdrawMember();
-
-            // Then
-            verify(memberRepository).delete(currentMember); // 회원 삭제 검증
-            assertThatCode(() -> memberRepository.delete(currentMember)).doesNotThrowAnyException(); // 예외 발생 여부 검증
-        }
-    }
+    class WithdrawMemberTests {}
 }
