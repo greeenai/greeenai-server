f5d6ffb51abab4fe9b5c450610b841139841a193
ohhamma <judyham10634@naver.com>
2025-04-08 13:27:57 +0900

    refactor: 불필요한 예외 처리 로직 제거

--- src/test/java/com/greeenai/greeenai/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/greeenai/greeenai/domain/member/service/MemberServiceTest.java
@@ -1,13 +1,14 @@
 package com.greeenai.greeenai.domain.member.service;
 
 import static com.greeenai.greeenai.global.common.constant.TestConstants.*;
-import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.greeenai.greeenai.domain.member.domain.Member;
 import com.greeenai.greeenai.domain.member.repository.MemberRepository;
 import com.greeenai.greeenai.global.util.MemberUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,5 +30,19 @@ class MemberServiceTest {
 
     @Nested
     @DisplayName("회원 탈퇴 테스트")
-    class WithdrawMemberTests {}
+    class WithdrawMemberTests {
+        @Test
+        @DisplayName("회원 탈퇴 요청을 하면 회원을 삭제한다")
+        void withdrawMember_shouldDeleteCurrentMember() {
+            // Given
+            Member currentMember = Member.create(TEST_NAME, TEST_EMAIL, TEST_OAUTH_ID, TEST_OAUTH_PROVIDER);
+            when(memberUtil.getCurrentMember()).thenReturn(currentMember);
+
+            // When
+            memberService.withdrawMember();
+
+            // Then
+            verify(memberRepository).delete(currentMember);
+        }
+    }
 }
