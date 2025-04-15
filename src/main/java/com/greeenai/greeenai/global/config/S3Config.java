3f2f3089db66144d7e3291b271ad882c2c22eaa0
ohhamma <judyham10634@naver.com>
2025-04-15 15:28:26 +0900

    chore: s3 presigner 추가

--- src/main/java/com/greeenai/greeenai/global/config/S3Config.java
+++ src/main/java/com/greeenai/greeenai/global/config/S3Config.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,12 +19,26 @@ public class S3Config {
     private final S3Properties s3Properties;
 
     @Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        AwsCredentials awsCredentials = AwsBasicCredentials.create(
+                s3Properties.getCredentials().accessKey(),
+                s3Properties.getCredentials().secretKey());
+        return StaticCredentialsProvider.create(awsCredentials);
+    }
+
+    @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(s3Properties.getRegion()))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(
-                        s3Properties.getCredentials().accessKey(),
-                        s3Properties.getCredentials().secretKey())))
+                .credentialsProvider(awsCredentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(s3Properties.getRegion()))
+                .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 }
