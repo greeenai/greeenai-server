d78e58a8ba837034b09c9a0bae4bb5d7e5206be6
ohhamma <judyham10634@naver.com>
2025-04-29 15:17:53 +0900

    feat: 일기에 대한 질문과 답변 레포지토리 생성

---
+++ src/main/java/com/greeenai/greeenai/domain/diary/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.greeenai.greeenai.domain.diary.repository;
+
+import com.greeenai.greeenai.domain.diary.domain.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
---
+++ src/main/java/com/greeenai/greeenai/domain/diary/repository/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.greeenai.greeenai.domain.diary.repository;
+
+import com.greeenai.greeenai.domain.diary.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
