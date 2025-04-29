aab2e2ccd700d3dcfd1d802ca9957c6d78f0b404
ohhamma <judyham10634@naver.com>
2025-04-29 13:46:55 +0900

    feat: 현재 회원의 일기 목록 조회

--- src/main/java/com/greeenai/greeenai/domain/diary/service/DiaryService.java
+++ src/main/java/com/greeenai/greeenai/domain/diary/service/DiaryService.java
@@ -3,11 +3,15 @@ package com.greeenai.greeenai.domain.diary.service;
 import com.greeenai.greeenai.domain.diary.domain.Diary;
 import com.greeenai.greeenai.domain.diary.dto.DiaryResponse;
 import com.greeenai.greeenai.domain.diary.repository.DiaryRepository;
+import com.greeenai.greeenai.domain.member.domain.Member;
 import com.greeenai.greeenai.global.error.exception.CustomException;
+import com.greeenai.greeenai.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.greeenai.greeenai.global.error.exception.ErrorCode.*;
 
@@ -18,6 +22,14 @@ import static com.greeenai.greeenai.global.error.exception.ErrorCode.*;
 public class DiaryService {
 
 	private final DiaryRepository diaryRepository;
+	private final MemberUtil memberUtil;
+
+	public List<DiaryResponse> findAllDiaries() {
+		Member currentMember = memberUtil.getCurrentMember();
+		return currentMember.getDiaries().stream()
+				.map(DiaryResponse::from)
+				.toList();
+	}
 
 	public DiaryResponse findDiaryById(Long id) {
 		Diary diary = diaryRepository.findById(id)
