2ffdb6fbe6fcf97f3d958d1260f768950dd1c68b
ohhamma <judyham10634@naver.com>
2025-04-29 15:49:54 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/greeenai/greeenai/domain/diary/dto/QuestionResponse.java
+++ src/main/java/com/greeenai/greeenai/domain/diary/dto/QuestionResponse.java
@@ -1,6 +1,5 @@
 package com.greeenai.greeenai.domain.diary.dto;
 
-import com.greeenai.greeenai.domain.diary.domain.Answer;
 import com.greeenai.greeenai.domain.diary.domain.Question;
 
 public record QuestionResponse(
