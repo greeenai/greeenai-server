b159787faa9f53ce5040a272a4cc14a301f87149
ohhamma <judyham10634@naver.com>
2025-04-29 13:41:39 +0900

    feat: 일기 조회 및 삭제

---
+++ src/main/java/com/greeenai/greeenai/domain/diary/service/DiaryService.java
@@ -0,0 +1,31 @@
+package com.greeenai.greeenai.domain.diary.service;
+
+import com.greeenai.greeenai.domain.diary.domain.Diary;
+import com.greeenai.greeenai.domain.diary.dto.DiaryResponse;
+import com.greeenai.greeenai.domain.diary.repository.DiaryRepository;
+import com.greeenai.greeenai.global.error.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.greeenai.greeenai.global.error.exception.ErrorCode.*;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class DiaryService {
+
+	private final DiaryRepository diaryRepository;
+
+	public DiaryResponse findDiaryById(Long id) {
+		Diary diary = diaryRepository.findById(id)
+				.orElseThrow(() -> new CustomException(DIARY_NOT_FOUND));
+		return DiaryResponse.from(diary);
+	}
+
+	public void deleteDiaryById(Long id) {
+		diaryRepository.deleteById(id);
+	}
+}
