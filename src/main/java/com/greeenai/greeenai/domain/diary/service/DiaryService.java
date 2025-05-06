321c2e0ff858f89f011f99ff503209aff1d7f13b
ohhamma <judyham10634@naver.com>
2025-05-06 13:22:37 +0900

    feat: 일기 서비르 로직에 로그 추가

--- src/main/java/com/greeenai/greeenai/domain/diary/service/DiaryService.java
+++ src/main/java/com/greeenai/greeenai/domain/diary/service/DiaryService.java
@@ -42,15 +42,18 @@ public class DiaryService {
 
 	@Transactional
 	public DiaryWithQuestionsResponse createDiary(DiaryCreateRequest request) {
+		log.info("createDiary 시작 - entryDate: {}", request.entryDate());
 		Member currentMember = memberUtil.getCurrentMember();
 		Diary diary = Diary.create(null, request.entryDate(), currentMember);
 		diaryRepository.save(diary);
 		// TODO : AI에게 그림일기 생성용 사진 주고 질문 받아오기
+		log.info("createDiary 완료 - diaryId: {}", diary.getId());
 		return DiaryWithQuestionsResponse.from(diary);
 	}
 
 	@Transactional
 	public DiaryWithQuestionsAndAnswersResponse answerDiaryQuestions(Long diaryId, List<QuestionAnswerRequest> requests) {
+		log.info("answerDiaryQuestions 시작 - diaryId: {}, 답변 개수: {}", diaryId, requests.size());
 		requests.forEach(request -> {
 			Question question = questionRepository.findById(request.questionId())
 					.orElseThrow(() -> new CustomException(QUESTION_NOT_FOUND));
@@ -60,6 +63,7 @@ public class DiaryService {
 		// TODO : AI에게 질문 답변 묶음 보내주기
 		Diary diary = diaryRepository.findById(diaryId)
 				.orElseThrow(() -> new CustomException(DIARY_NOT_FOUND));
+		log.info("answerDiaryQuestions 완료 - diaryId: {}", diaryId);
 		return DiaryWithQuestionsAndAnswersResponse.from(diary);
 	}
 }
