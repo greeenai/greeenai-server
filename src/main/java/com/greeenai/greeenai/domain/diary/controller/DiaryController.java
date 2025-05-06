3edd810e976e5d7ef8422bc83846002eb5ca5f7c
chosw1002@naver.com <chosw1002@naver.com>
2025-05-06 15:02:35 +0900

    feat: 특정 일기 삭제 API 구현

---
+++ src/main/java/com/greeenai/greeenai/domain/diary/controller/DiaryController.java
@@ -0,0 +1,22 @@
+package com.greeenai.greeenai.domain.diary.controller;
+
+import com.greeenai.greeenai.domain.diary.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diaries")
+@RequiredArgsConstructor
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    @DeleteMapping("/{diaryId}")
+    public ResponseEntity<Void> deleteDiary(@PathVariable Long diaryId) {
+        diaryService.deleteDiary(diaryId);
+        return ResponseEntity.ok().build();
+    }
+}
