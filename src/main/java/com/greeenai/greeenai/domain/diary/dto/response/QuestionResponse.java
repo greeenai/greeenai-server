35009e762c6209d87567770318dd27eec819988d
ohhamma <judyham10634@naver.com>
2025-05-13 16:46:13 +0900

    refactor: 변경된 question 구조에 맞게 수정

--- src/main/java/com/greeenai/greeenai/domain/diary/dto/response/QuestionResponse.java
+++ src/main/java/com/greeenai/greeenai/domain/diary/dto/response/QuestionResponse.java
@@ -1,9 +1,19 @@
 package com.greeenai.greeenai.domain.diary.dto.response;
 
 import com.greeenai.greeenai.domain.diary.domain.Question;
+import java.util.List;
 
-public record QuestionResponse(Long id, String content) {
+public record QuestionResponse(Long id, String title, String caption, String prompt, List<OptionResponse> options) {
     public static QuestionResponse from(Question question) {
-        return new QuestionResponse(question.getId(), question.getPrompt());
+        return new QuestionResponse(
+                question.getId(),
+                question.getTitle(),
+                question.getCaption(),
+                question.getPrompt(),
+                getOptions(question));
+    }
+
+    private static List<OptionResponse> getOptions(Question question) {
+        return question.getOptions().stream().map(OptionResponse::from).toList();
     }
 }
