bc708d397eabd7c9445793ffa3661c50a88efd43
ohhamma <judyham10634@naver.com>
2025-05-18 15:29:07 +0900

    chore: 불필요한 @JsonProperty 제거

--- src/main/java/com/greeenai/greeenai/domain/ai/dto/response/GenerateDiaryResponse.java
+++ src/main/java/com/greeenai/greeenai/domain/ai/dto/response/GenerateDiaryResponse.java
@@ -1,5 +1,3 @@
 package com.greeenai.greeenai.domain.ai.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record GenerateDiaryResponse(@JsonProperty("diary") String diary) {}
+public record GenerateDiaryResponse(String diary) {}
