7f9286725104e3b994482b05be5c5a2fb37e1286
ohhamma <judyham10634@naver.com>
2025-05-19 03:05:30 +0900

    choreL spotless 적용

--- src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
+++ src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
@@ -12,16 +12,14 @@ public class WebClientConfig {
 
     @Bean
     public WebClient kaggomWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("https://kaggom.online")
+        return builder.baseUrl("https://kaggom.online")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 
     @Bean
     public WebClient aiWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://43.203.204.202:8000")
+        return builder.baseUrl("http://43.203.204.202:8000")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(config -> config.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB로 확장
