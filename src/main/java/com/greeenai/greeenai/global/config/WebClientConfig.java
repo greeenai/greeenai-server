d8d4615fab9b5ea2c73e782087ca1612bf37de1c
ohhamma <judyham10634@naver.com>
2025-05-20 12:34:00 +0900

    refactor: webclient properties로 base url 가져오기

--- src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
+++ src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
@@ -1,5 +1,7 @@
 package com.greeenai.greeenai.global.config;
 
+import com.greeenai.greeenai.global.property.WebClientProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -7,19 +9,22 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebClientConfig {
 
+    private final WebClientProperties webClientProperties;
+
     @Bean
     public WebClient kaggomWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://kaggom.online")
+        return builder.baseUrl(webClientProperties.getKaggomBaseUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 
     @Bean
     public WebClient aiWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://43.203.204.202:8000")
+        return builder.baseUrl(webClientProperties.getAiBaseUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(config -> config.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB로 확장
