2d56baed630f1339d996f11567a931578e09a1fc
ohhamma <judyham10634@naver.com>
2025-05-20 12:36:23 +0900

    refactor: static import하도록 수정

--- src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
+++ src/main/java/com/greeenai/greeenai/global/config/WebClientConfig.java
@@ -4,10 +4,11 @@ import com.greeenai.greeenai.global.property.WebClientProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.MediaType.*;
 
 @RequiredArgsConstructor
 @Configuration
@@ -18,14 +19,14 @@ public class WebClientConfig {
     @Bean
     public WebClient kaggomWebClient(WebClient.Builder builder) {
         return builder.baseUrl(webClientProperties.getKaggomBaseUrl())
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .build();
     }
 
     @Bean
     public WebClient aiWebClient(WebClient.Builder builder) {
         return builder.baseUrl(webClientProperties.getAiBaseUrl())
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(config -> config.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB로 확장
                         .build())
