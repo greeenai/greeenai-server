544e700e0b41b538a378798c2ba417bca77a615b
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2025-05-20 15:49:23 +0900

    fix: entryDate가 null인 경우 발생하는 문제 수정 (#65)

--- src/main/java/com/greeenai/greeenai/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/greeenai/greeenai/domain/diary/repository/DiaryRepository.java
@@ -1,12 +1,6 @@
 package com.greeenai.greeenai.domain.diary.repository;
 
 import com.greeenai.greeenai.domain.diary.domain.Diary;
-import com.greeenai.greeenai.domain.member.domain.Member;
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-
-    List<Diary> findAllByMemberAndEntryDate(Member member, LocalDate entryDate);
-}
+public interface DiaryRepository extends JpaRepository<Diary, Long>, DiaryCustomRepository {}
